253c8b1b8c2491b0fbbaeb0ebb0d40ce9a823661
Marco Fabbi <marco.fabbi94@gmail.com>
2020-05-27 22:21:44 +0200

    pojo

--- WORK/src/com/Begear/JavaClasses/Pojo.java
+++ WORK/src/com/Begear/JavaClasses/Pojo.java
@@ -1,5 +1,66 @@
 package com.Begear.JavaClasses;
 
-public class Pojo {
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
 
+@Entity
+@Table(name = "datauser")
+public class Pojo {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "ID")
+	private int ID;
+	@Column(name = "username")
+	private String username;
+	@Column(name = "email")
+	private String email;
+	@Column(name = "password")
+	private String password;
+	
+	
+	public int getID() {
+		return ID;
+	}
+	public String getUsername() {
+		return username;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setID(int iD) {
+		ID = iD;
+	}
+	public void setUsername(String username) {
+		this.username = username;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	public Pojo(int iD, String username, String email, String password) {
+		setID(iD);
+		setUsername(username);
+		setEmail(email);
+		setPassword(password);
+	}
+	public Pojo(String username, String email, String password) {
+		setUsername(username);
+		setEmail(email);
+		setPassword(password);
+	}
+	@Override
+	public String toString() {
+		return "Autorizzazione [ID=" + ID + ", username=" + username + ", email=" + email + ", password=" + password
+				+ "]";
+	}
+	
 }
